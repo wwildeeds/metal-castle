038c68099ccf0eb325fd27062cb6e6a1f46a0223
wwildeeds <wwildeeds@naver.com>
2023-05-03 17:29:25 +0900

    새로운 게임, 옵션 페이지 클래스 정의

---
+++ Assets/Scripts/UI/Login/NewGamePage.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace wwild.ui.login
+{
+    using wwild.common.itf;
+    public class NewGamePage : ContentPage, IContentPage
+    {
+        public int InstanceID { get; private set; }
+
+        protected override void Awake()
+        {
+            Init();
+        }
+
+        protected override void Start()
+        {
+            AddListeners();
+        }
+
+        protected override void Init()
+        {
+            InstanceID = this.GetInstanceID();
+        }
+
+        protected override void AddListeners()
+        {
+            base.AddListeners();
+        }
+
+        protected override void RemoveListeners()
+        {
+            base.RemoveListeners();
+        }
+
+        public void Show()
+        {
+            canvas.sortingOrder = MAX_ORDER;
+        }
+
+        public void Hide()
+        {
+            canvas.sortingOrder = MIN_ORDER;
+        }
+    }
+}
